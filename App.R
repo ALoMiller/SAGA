d133ce26c91c3edcb8cb9e829e07e447ab744859
Dan Hennen <daniel.hennen@noaa.gov>
2019-07-17 14:27:41 -0400

    bug fux

--- App.R
+++ App.R
@@ -355,7 +355,7 @@ server = function(input, output, session){
     
   observeEvent(input$runBtn,{ #if run button is pushed:
 
-    if(length(input$mychooser$right)>0) {
+    if(length(input$mychooser$right)==0) {
       showNotification("PLEASE CHOOSE AT LEAST ONE STRATUM!!"
             ,id="strataChosen",duration=NULL,type="error")
     } else removeNotification(id="strataChosen")    
